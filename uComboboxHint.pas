fe6595b1f6c76ef28cafafdd41323cddd23d60dc
agapov <agapov-am@ya.ru>
2023-04-17 11:57:23 +0500

    48

--- uComboboxHint.pas
+++ uComboboxHint.pas
@@ -5,7 +5,7 @@ interface
 uses
   cxGraphics, cxControls, cxLookAndFeels, cxHint, dxCustomHint, cxClasses,
   cxLookAndFeelPainters, cxContainer, cxEdit, dxSkinsCore, cxDropDownEdit,
-  cxTextEdit, cxMaskEdit, cxImageComboBox, dxScreenTip,47
+  cxTextEdit, cxMaskEdit, cxImageComboBox, dxScreenTip,48
   System.Classes, Vcl.ExtCtrls, System.SysUtils, System.Types, Vcl.Forms;
 
 const
